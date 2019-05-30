e94f724616a3d4f993289c11917e0a0cbf2c64b3
yangyonglong <729880220@qq.com>
2019-05-30 21:25:05 +0800

    静态代理、动态代理

--- src/main/java/com/czy/proxy/AutoProxy.java
+++ src/main/java/com/czy/proxy/AutoProxy.java
@@ -5,26 +5,28 @@ import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 
 /**
- /**
- * @className Demo
+ * /**
+ *
  * @author yangyonglong
- * @date 2019-05-26 16:52
  * @version 1.0
+ * @className Demo
+ * @date 2019-05-26 16:52
  **/
 public class AutoProxy implements InvocationHandler {
 
 
     private Object obj;
 
-    public Object newProxyInstance(Object tar){
+    public Object newProxyInstance(Object tar) {
         this.obj = tar;
-        return Proxy.newProxyInstance(obj.getClass().getClassLoader(),obj.getClass().getInterfaces(),this);
+        return Proxy.newProxyInstance(obj.getClass().getClassLoader(), obj.getClass().getInterfaces(), this);
     }
+
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
         Object object = null;
         System.out.println("动态代理执行前");
-        object = method.invoke(obj,args);
+        object = method.invoke(obj, args);
         System.out.println("动态代理执行完成");
         return object;
     }
