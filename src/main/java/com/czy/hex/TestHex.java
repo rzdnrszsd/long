5f904370154cc9966be4208239bc0ba8dac897fa
yangyonglong <729880220@qq.com>
2020-03-02 20:21:07 +0800

    ...

--- src/main/java/com/czy/hex/TestHex.java
+++ src/main/java/com/czy/hex/TestHex.java
@@ -1,8 +1,10 @@
 package com.czy.hex;
 
-import cn.hutool.core.util.HashUtil;
-import cn.hutool.core.util.HexUtil;
-import cn.hutool.core.util.StrUtil;
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.collection.CollectionUtil;
+import cn.hutool.core.util.*;
+import cn.hutool.cron.CronUtil;
+import com.czy.stream.User;
 
 /**
  * @author yangyonglong
@@ -19,7 +21,38 @@ public class TestHex {
 		int number = 12;
 		long hash1 = HashUtil.additiveHash(s, Integer.MAX_VALUE);
 
+
+		NumberUtil.decimalFormat("",11.1);
 //		StrUtil.trim();
+
+
+//		CronUtil.schedule("*/2 * * * * ", new Runnable() {
+//			@Override
+//			public void run() {
+//				System.out.println("....");
+//			}
+//		});
+
+		Object object = new User();
+		ReflectUtil.setFieldValue(object, "name", "盖伦");
+		System.out.println(object.toString());
+
+	}
+
+	public static void main(String[] args) {
+//		Object object = new User();
+//		ReflectUtil.setFieldValue(object, "name", "盖伦");
+//		System.out.println(object.toString());
+
+		User user = new User();
+		user.setAge(22);
+		user.setName("3232");
+		user.setFlag(true);
+
+		User copyUser = new User();
+		BeanUtil.copyProperties(user,copyUser,"name","age");
+
+		System.out.println(copyUser.toString());
 
 
 	}
--- src/main/java/com/czy/stream/User.java
+++ src/main/java/com/czy/stream/User.java
@@ -12,4 +12,5 @@ import lombok.Data;
 public class User {
 	private String name;
 	private int age;
+	private boolean flag;
 }
